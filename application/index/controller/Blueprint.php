c07b9052d99605aaedadd88f62622fe293cf0f2f
dengsuchuan <1373518279@qq.com>
2018-06-17 22:20:12 +0800

    删除注释信息

--- application/index/controller/Blueprint.php
+++ application/index/controller/Blueprint.php
@@ -11,7 +11,6 @@ class Blueprint extends Base
        $blueprintInfoCount = $blueprintInfo->total();
        $this->assign('blueprintInfo',$blueprintInfo);
        $this->assign('blueprintInfoCount',$blueprintInfoCount);
-       //添加注释
        return $this->view->fetch('blueprint-info');
     }
 
