e2c754f568267fa33d30e2bde2161fc530442cb0
dengsuchuan <1373518279@qq.com>
2018-06-18 00:01:08 +0800

    thinkphp

---
+++ application/index/controller/Personnel.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dengs
+ * Date: 2018/6/7 0007
+ * Time: 23:20
+ */
+
+namespace app\index\controller;
+
+
+use app\index\common\controller\Base;
+
+class Personnel extends Base
+{
+    public function adminList(){
+        return $this->view->fetch('admin-list');
+    }
+
+    public function workerList(){
+        return $this->view->fetch('worker-list');
+    }
+
+    public function clientList(){
+        return $this->view->fetch('client-list');
+    }
+
+    public function authorityClass(){
+        return $this->view->fetch('authority-class');
+    }
+}
