161cb11d43db2f10d453fcb926e81eff28ca30cc
dengsuchuan <1373518279@qq.com>
2018-07-06 20:30:33 +0800

    完善外图信息，添加外图增加界面，添加明细增加界面

--- runtime/temp/d387ecccc3f058954b8d1d5fef870e6d.php
+++ runtime/temp/d387ecccc3f058954b8d1d5fef870e6d.php
@@ -1,4 +1,4 @@
-<?php /*a:2:{s:81:"D:\WebServer\www\project\Hy\application\index\view\blueprint\blueprint-infos.html";i:1529424503;s:69:"D:\WebServer\www\project\Hy\application\index\view\public\header.html";i:1528981768;}*/ ?>
+<?php /*a:2:{s:81:"D:\WebServer\www\project\Hy\application\index\view\blueprint\blueprint-infos.html";i:1529890286;s:69:"D:\WebServer\www\project\Hy\application\index\view\public\header.html";i:1528981768;}*/ ?>
 ﻿<!doctype html>
 <html lang="en">
 <head>
@@ -50,182 +50,182 @@
             <div class="layui-form-item">
                 <label class="layui-form-label">明细编号</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" title="无法编辑" value="<?php echo htmlentities($blueprintInfo['drawing_detail_id']); ?>" disabled>
+                    <input type="text" class="layui-input" title="无法编辑" value="<?php echo htmlentities($blueprintInfo['drawing_detail_id']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">公司编号</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" title="无法编辑" value="<?php echo htmlentities($blueprintInfo['drawing_internal_id']); ?>" disabled>
+                    <input type="text" class="layui-input" title="无法编辑" value="<?php echo htmlentities($blueprintInfo['drawing_internal_id']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">外图编号</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['drawing_externa_id']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['drawing_externa_id']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">图纸名称</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['drawing_name']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['drawing_name']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">材料类型</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['material']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['material']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">热处理类型</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['heat_treatment']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['heat_treatment']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">图纸类型</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['drawing_type']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['drawing_type']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">客户类型</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['client_id']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['client_id']); ?>"  >
                 </div>
             </div>
             <!--<div class="layui-form-item">-->
                 <!--<label class="layui-form-label">是否批量</label>-->
                 <!--<div class="layui-input-block">-->
-                    <!--<input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['if_batch']); ?>" disabled>-->
+                    <!--<input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['if_batch']); ?>"  >-->
                 <!--</div>-->
             <!--</div>-->
             <div class="layui-form-item">
                 <label class="layui-form-label">是否批量</label>
                 <div class="layui-input-block">
-                    <input type="checkbox" checked="" name="open" lay-skin="switch" lay-filter="switchTest" lay-text="是|否" disabled>
+                    <input type="checkbox" checked="" name="open" lay-skin="switch" lay-filter="switchTest" lay-text="是|否"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">材料形状</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['material_type']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['material_type']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">厚度</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['thickness_Dia']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['thickness_Dia']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">长度尺寸</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['length_dim']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['length_dim']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">宽度尺寸</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['width_dim']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['width_dim']); ?>"  >
                 </div>
             </div>
             <!--<div class="layui-form-item">-->
                 <!--<label class="layui-form-label">保证厚度</label>-->
                 <!--<div class="layui-input-block">-->
-                    <!--<input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['if_thickness']); ?>" disabled>-->
+                    <!--<input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['if_thickness']); ?>"  >-->
                 <!--</div>-->
             <!--</div>-->
             <!--<div class="layui-form-item">-->
                 <!--<label class="layui-form-label">保证长度</label>-->
                 <!--<div class="layui-input-block">-->
-                    <!--<input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['if_length']); ?>" disabled>-->
+                    <!--<input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['if_length']); ?>"  >-->
                 <!--</div>-->
             <!--</div>-->
             <!--<div class="layui-form-item">-->
                 <!--<label class="layui-form-label">保证宽度</label>-->
                 <!--<div class="layui-input-block">-->
-                    <!--<input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['if_width']); ?>" disabled>-->
+                    <!--<input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['if_width']); ?>"  >-->
                 <!--</div>-->
             <!--</div>-->
             <div class="layui-form-item">
                 <label class="layui-form-label">保证厚度</label>
                 <div class="layui-input-block">
-                    <input type="checkbox" checked="" name="open" lay-skin="switch" lay-filter="switchTest" lay-text="是|否" disabled>
+                    <input type="checkbox" checked="" name="open" lay-skin="switch" lay-filter="switchTest" lay-text="是|否"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">保证长度</label>
                 <div class="layui-input-block">
-                    <input type="checkbox" checked="" name="open" lay-skin="switch" lay-filter="switchTest" lay-text="是|否" disabled>
+                    <input type="checkbox" checked="" name="open" lay-skin="switch" lay-filter="switchTest" lay-text="是|否"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">保证宽度</label>
                 <div class="layui-input-block">
-                    <input type="checkbox" checked="" name="open" lay-skin="switch" lay-filter="switchTest" lay-text="是|否" disabled>
+                    <input type="checkbox" checked="" name="open" lay-skin="switch" lay-filter="switchTest" lay-text="是|否"  >
                 </div>
             </div>
 
             <div class="layui-form-item">
                 <label class="layui-form-label">排料数量</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['layout_qty']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['layout_qty']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">材料要求</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['material_requirements']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['material_requirements']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">产品加工费</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['product_mfg_cost']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['product_mfg_cost']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">产品报价</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['product_quotation']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['product_quotation']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">产品实价</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['product_real_price']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['product_real_price']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">创建人</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['create_name']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['create_name']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">最后修改人</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['modify_name']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['modify_name']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">创建时间</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['create_time']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['create_time']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">修改时间</label>
                 <div class="layui-input-block">
-                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['update_time']); ?>" disabled>
+                    <input type="text" class="layui-input" value="<?php echo htmlentities($blueprintInfo['update_time']); ?>"  >
                 </div>
             </div>
             <div class="layui-form-item layui-form-text">
                 <label class="layui-form-label">备注</label>
                 <div class="layui-input-block">
-                    <textarea class="layui-textarea" disabled> <?php echo htmlentities($blueprintInfo['remark']); ?> </textarea>
+                    <textarea class="layui-textarea"  > <?php echo htmlentities($blueprintInfo['remark']); ?> </textarea>
                 </div>
             </div>
 
