a988dee07cad7f5a7eb84a199717b956432eac4e
w王恒兵 <1781634625@qq.com>
2018-07-10 00:31:00 +0800

    任务提交：
    时间：2017年7月10
    提交人：王恒兵
    日志：完成对图纸明细的修改操作
    完成情况：OK

--- application/index/controller/Blueprint.php
+++ application/index/controller/Blueprint.php
@@ -4,6 +4,7 @@ namespace app\index\controller;
 use app\index\model\BlueprintInfo;
 use app\index\common\controller\Base;
 use app\index\model\BlueprintOutside;
+use think\facade\Request;
 
 class Blueprint extends Base
 {
@@ -16,7 +17,28 @@ class Blueprint extends Base
        return $this->view->fetch('blueprint-info');
     }
     //--|--|明细具体项目详情
-    public function blueprintInfos($id){
+    public function blueprintInfos(){
+        $id = input('id');
+        //如果是POST请求的话就代表是执行修改操作
+        if(Request::isAjax()){
+            //获取提交过来的所有数据
+            $data = Request::post();
+            //前台的开关按钮  如果开启 则是1  关闭的话为0
+            $data['if_batch'] = isset($data['if_batch']) ? '1': '0';
+            $data['if_thickness'] = isset($data['if_thickness']) ? '1': '0';
+            $data['if_length'] = isset($data['if_length']) ? '1': '0';
+            $data['if_width'] = isset($data['if_width']) ? '1': '0';
+            //获取提交过来的ID
+            $id = $data['id'];
+            unset($data['id']);
+            //执行更新操作  使用的模型更新  成功失败都会返回为当前模型的所以数据
+           $res =  BlueprintInfo::update($data,['id'=>$id]);
+           if($res){
+               return json(1);
+           }else{
+               return json(0);
+           }
+        }
         $blueprintInfo = BlueprintInfo::where('drawing_detail_id',$id)->find();
         $this->assign('blueprintInfo',$blueprintInfo);
         return $this->view->fetch('blueprint-infos');
