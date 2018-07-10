a251c7f99b9c7e7e8ac7ff929f29932b6d77a796
dengsuchuan <1373518279@qq.com>
2018-07-10 15:08:26 +0800

    实现添加外图编号

--- application/index/controller/Blueprint.php
+++ application/index/controller/Blueprint.php
@@ -10,14 +10,36 @@ class Blueprint extends Base
 {
     //--|图纸明细控制器以及相关子控制器
     public function blueprintInfo(){
-       $blueprintInfo = BlueprintInfo::order('create_time','desc')->paginate(25);
-       $blueprintInfoCount = $blueprintInfo->total();
-       $this->assign('blueprintInfo',$blueprintInfo);
-       $this->assign('blueprintInfoCount',$blueprintInfoCount);
-       return $this->view->fetch('blueprint-info');
+        $blueprintInfo = BlueprintInfo::order('create_time', 'desc')->paginate(25);
+        $blueprintInfoCount = $blueprintInfo->total();
+        $this->assign('blueprintInfo', $blueprintInfo);
+        $this->assign('blueprintInfoCount', $blueprintInfoCount);
+        return $this->view->fetch('blueprint-info');
     }
     //--|--|明细具体项目详情
-    public function blueprintInfos($id){
+    public function blueprintInfos()
+    {
+        $id = input('id');
+        //如果是POST请求的话就代表是执行修改操作
+        if (Request::isAjax()) {
+            //获取提交过来的所有数据
+            $data = Request::post();
+            //前台的开关按钮  如果开启 则是1  关闭的话为0
+            $data['if_batch'] = isset($data['if_batch']) ? '1' : '0';
+            $data['if_thickness'] = isset($data['if_thickness']) ? '1' : '0';
+            $data['if_length'] = isset($data['if_length']) ? '1' : '0';
+            $data['if_width'] = isset($data['if_width']) ? '1' : '0';
+            //获取提交过来的ID
+            $id = $data['id'];
+            unset($data['id']);
+            //执行更新操作  使用的模型更新  成功失败都会返回为当前模型的所以数据
+            $res = BlueprintInfo::update($data, ['id' => $id]);
+            if ($res) {
+                return json(1);
+            } else {
+                return json(0);
+            }
+        }
         $blueprintInfo = BlueprintInfo::where('drawing_detail_id',$id)->find();
         $this->assign('blueprintInfo',$blueprintInfo);
         return $this->view->fetch('blueprint-infos');
@@ -39,7 +61,6 @@ class Blueprint extends Base
         $this->assign('blueprintOutside',$blueprintOutside);
         $this->assign('blueprintOutsideCount',$blueprintOutsideCount);
         return $this->view->fetch('blueprint-outside');
-
     }
     //--|--|添加外图视图生成器
     public function addDrawingExterna(){
@@ -47,7 +68,6 @@ class Blueprint extends Base
         $blueprintOutside = BlueprintOutside::where('drawing_external_id',"LIKE","W180706-%")
             ->order("drawing_external_id",'DESC')
             ->find();
-        $this->assign("blueprintOutsideList",$blueprintOutside);
         if($blueprintOutside){
             $stdClassTemp = json_decode($blueprintOutside);
             $stdClassJson  =json_encode($stdClassTemp); //把她转换为json字符串
@@ -81,6 +101,7 @@ class Blueprint extends Base
     public function addDrawingDetial(){
         return $this->view->fetch('add-drawing-detial');
     }
+
 
     public function blueprintInterior(){
 
