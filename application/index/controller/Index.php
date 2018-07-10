4dc97c56cf865ebcc38fccaccca474f07dc47844
知否丶 <vclinlin@vip.qq.com>
2018-07-10 15:40:57 +0800

    删除事件

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,6 +3,9 @@ namespace app\index\controller;
 
 
 use app\index\common\controller\Base;
+use app\index\model\logs;
+use think\Db;
+use think\Request;
 
 class Index extends Base
 {
@@ -12,5 +15,34 @@ class Index extends Base
 
     public function welcome(){
         return $this->view->fetch('welcome');
+    }
+    public function DelAll(Request $request)  //用于删除
+    {
+        $Ary = $request->post();
+        //总条数
+        $count = count($Ary['data']);
+        $sum = 0;
+        $model = new logs();
+        for($i=0;$i<$count;$i++)
+        {
+            if(!Db::table($Ary['table'])->where(["Id"=>$Ary['data'][$i]])->delete())
+            {
+                continue;//失败不统计
+            }
+            //计数
+            $sum++;
+            //写入日志
+        }
+        if($sum>0) {   //产生有效时间再记录日志
+            $model->save([
+                "date" => time(),
+                "msg" => "删除" . $Ary['table'] . "表中记录" . $sum . "条"
+            ]);
+        }
+        echo json_encode($data=[
+            "state"=>200,
+            "message"=>"选中".$count."条记录,共".$sum."条删除成功"
+        ]);
+        return;
     }
 }
---
+++ application/index/model/logs.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 17424
+ * Date: 2018/7/10
+ * Time: 15:38
+ */
+
+namespace app\index\model;
+
+
+use think\Model;
+
+class logs extends Model
+{
+
+}
