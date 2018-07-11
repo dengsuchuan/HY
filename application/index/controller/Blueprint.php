1f78d97afb3795c5df14c08e2be759897a08b05c
w王恒兵 <1781634625@qq.com>
2018-07-11 09:30:16 +0800

    优化图纸明细搜索

--- application/index/controller/Blueprint.php
+++ application/index/controller/Blueprint.php
@@ -10,6 +10,7 @@ class Blueprint extends Base
     //--|图纸明细控制器以及相关子控制器
     public function blueprintInfo(){
         //判断是否为post提交请求。如果是，就代表是搜索。
+        $blueprintKeyInfo = BlueprintInfo::order('create_time', 'desc')->select();
         if(Request::isPost()){
             $data = Request::post();
             $blueprintInfo = BlueprintInfo::order('create_time', 'desc')
@@ -18,14 +19,12 @@ class Blueprint extends Base
                 ->whereOr(['drawing_externa_id'=>$data['modules']])
                 ->paginate(5);
             $blueprintInfoCount = $blueprintInfo->total();
-            $blueprintKeyInfo = BlueprintInfo::order('create_time', 'desc')->select();
             $this->assign('blueprintInfo', $blueprintInfo);
             $this->assign('blueprintInfoCount', $blueprintInfoCount);
             $this->assign('blueprintKeyInfo', $blueprintKeyInfo);
             return $this->view->fetch('blueprint-info');
         }
         $blueprintInfo = BlueprintInfo::order('create_time', 'desc')->paginate(25);
-        $blueprintKeyInfo = BlueprintInfo::order('create_time', 'desc')->select();
         $blueprintInfoCount = $blueprintInfo->total();
         $this->assign('blueprintInfo', $blueprintInfo);
         $this->assign('blueprintInfoCount', $blueprintInfoCount);
