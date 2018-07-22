09e5f40c87646bcfabe6836afa0dbeae9e2436fa
dengsuchuan <1373518279@qq.com>
2018-07-22 22:43:29 +0800

    修改公司编号和添加模具管理

--- application/index/controller/Blueprint.php
+++ application/index/controller/Blueprint.php
@@ -166,76 +166,37 @@ class Blueprint extends Base
     }
 
     //获取M和P的值
-    public function getMP($data){
-        $strM = "M".date('y').date('m').date('d');
+    public function getMP(){
         $strP = "P".date('y').date('m').date('d');
-        $mArray = ComparnyM::where("mid","like",$strM."-%")->select(); //查找M的数据
         $pArray = ComparnyP::where("pid","like",$strP."-%")->select(); //查找P的数据
 
-        if($data == "M"){/*获取有多少M*/
-
-            if(count($mArray)){ /*count($mArray)等于0，则执行else的内容*/
-                //拆分字符串为数组
-                foreach ($mArray as $value){
-                    $midArray[] = explode("-",$value['mid']);
-                }
-                //拆分单独的序号出来
-                foreach ($midArray as $value){
-                    $midOrder[] = $value[1];
-                }
-                //找到最大的序号
-                $maxMid = max($midOrder);
-
-                //合成将要生成的M编号
-                $tempMid = $strM."-".($maxMid+1);
-                //合成新的公司编号
-                $companyNumber = $tempMid."-1";
-
-                $tempArray = [
-                    "cou"=>count($mArray),
-                    "mes"=>"存在",
-                    "mid"=>$midArray,
-                    "max"=>$maxMid,
-                    "companyNumber"=>$tempMid
-                ];
-            }else{
-                $tempArray = ["cou"=>0,"mes"=>"不存在","companyNumber"=>$strM."-1"];
+        if(count($pArray)){ /*count($mArray)等于0，则执行else的内容*/
+            //拆分字符串为数组
+            foreach ($pArray as $value){
+                $pidArray[] = explode("-",$value['pid']);
             }
-            return $tempArray;
-
-
-        }else if ($data == "P"){//获取有多少P
-            if(count($pArray)){ /*count($mArray)等于0，则执行else的内容*/
-                //拆分字符串为数组
-                foreach ($pArray as $value){
-                    $pidArray[] = explode("-",$value['pid']);
-                }
-                //拆分单独的序号出来
-                foreach ($pidArray as $value){
-                    $pidOrder[] = $value[1];
-                }
-                //找到最大的序号
-                $maxPid = max($pidOrder);
-
-
-                //合成将要生成的P编号//
-                $tempPid = $strP."-".($maxPid+1);
-
-                $tempArray = [
-                    "cou"=>count($pArray),
-                    "mes"=>"存在",
-                    "pids"=>$pidArray,
-                    "max"=>$maxPid,
-                    "companyNumber"=>$tempPid
-                ];
-            }else{
-                $tempArray = ["cou"=>0,"mes"=>"不存在","companyNumber"=>$strP."-1"];
+            //拆分单独的序号出来
+            foreach ($pidArray as $value){
+                $pidOrder[] = $value[1];
             }
-            return $tempArray;
-        }else{//获取有多少MP
-            return "MP";
+            //找到最大的序号
+            $maxPid = max($pidOrder);
 
+
+            //合成将要生成的P编号//
+            $tempPid = $strP."-".($maxPid+1);
+
+            $tempArray = [
+                "cou"=>count($pArray),
+                "mes"=>"存在",
+                "pids"=>$pidArray,
+                "max"=>$maxPid,
+                "companyNumber"=>$tempPid
+            ];
+        }else{
+            $tempArray = ["cou"=>0,"mes"=>"不存在","companyNumber"=>$strP."-1"];
         }
+        return $tempArray;
     }
 
     //拿到新的公司编号值
@@ -318,19 +279,11 @@ class Blueprint extends Base
         $this->assign("detailArray",$detailArray);
         /*----------------公司编号处理----------------------*/
         //先获取所有的模具M
-        $allM = ComparnyM::order("id","desc")->select();
         $allP = ComparnyP::order("id","desc")->select();
-        $this->assign("allM",$allM);
         $this->assign("allP",$allP);
 
-        $midArray = $this->getMP("M");
         $pidArray = $this->getMP("P");
-        //print_r($tempArray);
-        $this->assign("midArray",$midArray);
         $this->assign("pidArray",$pidArray);
-
-
-
 
         /*----------------公司编号处理----------------------*/
 
--- application/index/controller/Mould.php
+++ application/index/controller/Mould.php
@@ -10,10 +10,65 @@ namespace app\index\controller;
 
 
 use app\index\common\controller\Base;
+use app\index\model\ComparnyM;
+use app\index\model\ComparnyP;
 
 class Mould extends Base
 {
     public function mould(){
         return $this->view->fetch('mould');
+    }
+
+    public function mouldAdd(){
+        /*----------------公司编号处理----------------------*/
+        //先获取所有的模具M
+        $allM = ComparnyM::order("id","desc")->select();
+        $allP = ComparnyP::order("id","desc")->select();
+        $this->assign("allM",$allM);
+        $this->assign("allP",$allP);
+
+        $midArray = $this->getMP("M");
+        $pidArray = $this->getMP("P");
+        //print_r($tempArray);
+        $this->assign("midArray",$midArray);
+        $this->assign("pidArray",$pidArray);
+
+
+        return $this->view->fetch("mould-add");
+    }
+
+    //获取M和P的值
+    public function getMP($data){
+        $strM = "M".date('y').date('m').date('d');
+        $mArray = ComparnyM::where("mid","like",$strM."-%")->select(); //查找M的数据
+
+        if(count($mArray)){ /*count($mArray)等于0，则执行else的内容*/
+            //拆分字符串为数组
+            foreach ($mArray as $value){
+                $midArray[] = explode("-",$value['mid']);
+            }
+            //拆分单独的序号出来
+            foreach ($midArray as $value){
+                $midOrder[] = $value[1];
+            }
+            //找到最大的序号
+            $maxMid = max($midOrder);
+
+            //合成将要生成的M编号
+            $tempMid = $strM."-".($maxMid+1);
+            //合成新的公司编号
+            $companyNumber = $tempMid."-1";
+
+            $tempArray = [
+                "cou"=>count($mArray),
+                "mes"=>"存在",
+                "mid"=>$midArray,
+                "max"=>$maxMid,
+                "companyNumber"=>$tempMid
+            ];
+        }else{
+            $tempArray = ["cou"=>0,"mes"=>"不存在","companyNumber"=>$strM."-1"];
+        }
+        return $tempArray;
     }
 }
