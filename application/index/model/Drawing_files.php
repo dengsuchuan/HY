91431cff587e71989a5af491ae272c6c26053777
知否丶 <vclinlin@vip.qq.com>
2018-08-02 23:09:28 +0800

    Merge branch 'master' of D:\Vc_PHP\Apache24\htdocs\2018\Hy with conflicts.

---
+++ application/index/model/Drawing_files.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 17424
+ * Date: 2018/7/31
+ * Time: 11:17
+ */
+
+namespace app\index\model;
+
+use think\Model;
+
+class Drawing_files extends Model
+{
+    protected $pk = 'id';
+    protected $table = 'hy_drawing_files';
+
+
+}
