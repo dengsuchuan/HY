1bc7f01bbd53b028fac4348454409519544df13f
dengsuchuan <1373518279@qq.com>
2018-08-07 01:42:22 +0800

    新加客户管理

---
+++ application/index/controller/Client.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * 员工控制器
+ * User: @ 若雨
+ * Date: 2018/7/30
+ * Time: 19:28
+ */
+
+namespace app\index\controller;
+
+use app\index\common\controller\Base;
+use app\index\model\Client as ClientModel;
+use function PHPSTORM_META\elementType;
+use think\facade\Request;
+
+class Client extends Base
+{
+
+    //渲染列表
+    public function clientInfo(){
+
+        $clientInfo  = ClientModel::order('create_time', 'desc')->paginate('15');
+        $this->assign(['clientInfo' =>   $clientInfo]);
+        return $this->view->fetch('client-info');
+    }
+    public function clientAdd(){
+        if(Request::isAjax()){
+            $data = Request::post();
+            $info = ClientModel::create($data);
+            if($info){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+        return $this->view->fetch('client-add');
+    }
+    public function clientEdit(){
+        if(Request::isAjax()) {
+            $data = Request::post();
+            $id = $data['id'];
+            unset($data['id']);
+
+            $info = ClientModel::update($data,['id'=>$id]);
+            if ($info){
+                return json(1);
+            }else{
+                return json(0);
+            }
+        }
+
+        $id = intval(input('id'));
+        $clientRow = ClientModel::get($id);
+        $this->assign('clientRow',$clientRow);
+        return $this->view->fetch('client-edit');
+
+    }
+
+    public function delete(){
+        $id = intval(input('id'));
+        $info =  ClientModel::where('id',$id)->delete();
+        if($info){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+}
