7250bce7f1b675be1a146b275c4f2fee5efc9fef
dengsuchuan <dsccsd20IIIIIDSC>
2018-09-06 18:33:02 +0800

    报价

--- application/index/controller/Quoted.php
+++ application/index/controller/Quoted.php
@@ -9,9 +9,21 @@ namespace app\index\controller;
 
 use app\index\common\controller\Base;
 class Quoted extends  Base{
-    public function quotedInfo(){
+    public function quotedInfoType1(){
+        $order_id = input("id");
+        $this->view->assign("order_id",$order_id);
         return $this->view->fetch("view-quoted-info");
     }
 
+    public function quotedInfoType2(){
+        $order_id = input("id");
+        $this->view->assign("order_id",$order_id);
+        return $this->view->fetch("view-quoted-info");
+    }
 
+    public function quotedInfoType3(){
+        $order_id = input("id");
+        $this->view->assign("order_id",$order_id);
+        return $this->view->fetch("view-quoted-info");
+    }
 }
