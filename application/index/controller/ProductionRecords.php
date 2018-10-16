78226598d49a88a2d1d9a7e9e1befc8074afa0d0
dengsuchuan <1373518279@qq.com>
2018-10-16 21:34:40 +0800

    签名

--- application/index/controller/ProductionRecords.php
+++ application/index/controller/ProductionRecords.php
@@ -12,16 +12,23 @@ namespace app\index\controller;
 use app\index\common\controller\Base;
 use app\index\model\EquipmentInfo;
 use app\index\model\ProductionRecordsModel;
+use app\index\model\ProductLog;
 use app\index\model\ProductProcess;
 use app\index\model\Employee;
+use think\facade\Request;
 
 
 class ProductionRecords extends Base
 {
+
     public function index(){
         $task_id = input('task_id');
+        $productLog = ProductLog::order('create_time', 'asc')->paginate(25);
+        $productLogCount = $productLog->total();
         $this->view->assign([
-            'task_id'=>$task_id
+            'task_id'=>$task_id,
+            'productLog'=>$productLog,
+            'productLogCount'=>$productLogCount
         ]);
         return $this->view->fetch();
     }
@@ -42,4 +49,80 @@ class ProductionRecords extends Base
         ]);
         return $this->view->fetch();
     }
+
+    public function saveLog(){
+        if(Request::isAjax()){
+            $data=Request::post();
+            //hy_product_log
+            $info = ProductLog::create($data);
+            if($info){
+                return json(1);
+            }else{
+                return json(0);
+            }
+        }
+    }
+
+    public function sign(){
+        if(Request::isAjax()){
+            $data = Request::post();
+            $log_id = $data['where'];
+            $nameKey = $data['nameKey'];
+            $dateKey = $data['dateKey'];
+            $username = session('user.user_name');
+            $date = date('Y-m-d');
+            $array=[
+                "$nameKey"=>$username,
+                "$dateKey"=>$date
+            ];
+            if(ProductLog::where("log_id",$log_id)->update($array)){
+                return json(1);
+            }else{
+                return json(0);
+            }
+        }
+    }
+    public function signDown(){
+        if(Request::isAjax()){
+            $data = Request::post();
+            $log_id = $data['where'];
+            $nameKey = $data['nameKey'];
+            $dateKey = $data['dateKey'];
+            $username = "";
+            $date = "";
+            $array=[
+                "$nameKey"=>$username,
+                "$dateKey"=>$date
+            ];
+            if(ProductLog::where("log_id",$log_id)->update($array)){
+                return json(1);
+            }else{
+                return json(0);
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ application/index/model/ProductLog.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 工艺表模型
+ * User: SYSTEM
+ * Date: 2018/7/11
+ * Time: 23:43
+ */
+
+namespace app\index\model;
+
+
+use think\Model;
+
+class ProductLog extends Model
+{
+    protected $pk = 'log_id';
+    protected $table = 'hy_product_log';
+    protected $autoWriteTimestamp = true;//自动时间戳
+    protected $createTime = 'create_time';
+    protected $updateTime = 'update_time';
+    protected $dateFormat = 'Y-m-d';
+
+}
